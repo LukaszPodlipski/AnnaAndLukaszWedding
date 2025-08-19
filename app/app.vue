57b35ddc52bbb50401511b2c9c83168250163990
Łukasz Podlipski <lukaszpodlipskikontakt@gmail.com>
2025-08-19 22:35:37 +0200

    Add og image tags

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+  useSeoMeta({
+    title: 'Anna i Łukasz - Wesele',
+    description: 'Wesele Anny i Łukasza',
+    ogTitle: 'Anna i Łukasz - Wesele',
+    ogDescription: 'Wesele Anny i Łukasza',
+    ogImage: 'https://www.annailukasz.pl/hero-left-image.jpg',
+    ogUrl: 'https://www.annailukasz.pl',
+    twitterCard: 'summary_large_image',
+    twitterTitle: 'Anna i Łukasz - Wesele',
+    twitterDescription: 'Wesele Anny i Łukasza',
+    twitterImage: 'https://www.annailukasz.pl/hero-left-image.jpg',
+  });
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage></NuxtPage>
